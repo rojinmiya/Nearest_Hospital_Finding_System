5677a897234972cd8fed7b307f90c28b1ba09efc
rojinmiya <miyarojin557@gmail.com>
2019-12-03 11:14:20 +0545

    Modified

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+const path = require('path');
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'rojin',
+    password: 'rojin',
+    database: 'alumnieventmanagementsystem'
+  },
+  migrations: {
+    tableName: 'migrations',
+    directory: path.resolve(__dirname, './migrations'),
+  },
+  useNullAsDefault: false
+};
